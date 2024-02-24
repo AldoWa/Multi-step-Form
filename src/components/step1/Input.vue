a474afbe23445b0e02ca390fcc646c96742b0367
Aldo Wanderley Costa Junior <aldowanderley11@gmail.com>
2024-02-24 14:15:40 -0300

    :sparkles: creating inputs

--- src/components/step1/Input.vue
+++ src/components/step1/Input.vue
@@ -1,9 +1,53 @@
 <template>
-  <div>
-    Input
+  <div class="flex flex-col">
+    <label 
+      htmlFor="input"
+      class="text-sm font-medium text-[#111827] mb-2"
+    >
+      {{ label }}
+    </label>
+    <input
+      class="
+        border-2 
+        border-gray-300 
+        rounded-[10px] 
+        h-10
+        p-2 
+        focus:outline-none 
+        focus:ring-2 
+        focus:ring-[#FC6C4C] 
+        focus:border-transparent
+        placeholder-gray-400
+        hover:cursor-pointer
+      "
+      :type="type" 
+      name="input"
+      id="input"
+      :autocomplete="autocomplete"
+      @input="emitValue"
+      :value="modelValue"
+      :placeholder="placeholder"
+    >
   </div>
 </template>
 
 <script setup lang="ts">
+
+
+interface InputProps {
+  label: string
+  modelValue: string
+  autocomplete?: string;
+  type: string;
+  placeholder: string;
+}
+
+defineProps<InputProps>()
+const emit = defineEmits(['update:modelValue'])
+
+function emitValue(e: Event) {
+  const target = e.target as HTMLInputElement
+  emit('update:modelValue', target.value)
+}
 
 </script>
--- src/components/step1/Step1.vue
+++ src/components/step1/Step1.vue
@@ -1,9 +1,44 @@
 <template>
-  <div>
-    Step1
+  <div class="grid grid-cols-2 gap-6 mt-6">
+    <Input 
+      label="Nome" 
+      v-model="personalInformation.nome"
+      type="text"
+      placeholder="Richard Rasmunse"
+    />
+    <Input 
+      label="Email Address" 
+      v-model="personalInformation.email"
+      type="email"
+      autocomplete="email"
+      placeholder="name@email.com"
+    />
+    <Input 
+      label="Phone Number" 
+      v-model="personalInformation.phone"
+      type="text"
+      autocomplete="tel"
+      placeholder="(79) 988129628"
+    />
+    <Input 
+      label="Github Link" 
+      v-model="personalInformation.github"
+      type="text"
+      autocomplete="url"
+      placeholder="github.com/richardrasmunse"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+import Input from './Input.vue';
+
+const personalInformation = ref({
+  nome: '',
+  email: '',
+  phone: '',
+  github: '',
+})
 
 </script>
