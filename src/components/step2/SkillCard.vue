a275bf396ab8c826a6c3a1a2800f590de11abb58
Aldo Wanderley Costa Junior <aldowanderley11@gmail.com>
2024-02-26 13:05:59 -0300

    :sparkles: adding icons

---
+++ src/components/step2/SkillCard.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+  interface SkillCardProps {
+    title: string
+    description: string
+  }
+
+  defineProps<SkillCardProps>()
+</script>
